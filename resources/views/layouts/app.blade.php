5778fb8b5cd928372eb6cde65164df513f222099
Sinan Baymammadli <sinan.baymammadli@gmail.com>
2017-11-17 02:30:21 +0400

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Aznur Kabel</title>
+    <title>Aznur Kabel | </title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
